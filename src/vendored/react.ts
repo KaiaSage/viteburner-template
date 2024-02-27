e4c00098bff5f1c5aba5b365fc15f3bdc20ac78b
KaiaSage <106801009+KaiaSage@users.noreply.github.com>
2024-02-27 04:53:56 -0800

    Template with React, node_modules, and CSS support

---
+++ src/vendored/react.ts
@@ -0,0 +1,35 @@
+
+// Credit to https://github.com/ZachHaber:
+// https://github.com/Tanimodori/viteburner/issues/18#issuecomment-1902753427
+
+export default React;
+export type * from 'react';
+export const {
+  Children,
+  Fragment,
+  Component,
+  Profiler,
+  PureComponent,
+  cloneElement,
+  createContext,
+  createElement,
+  createFactory,
+  createRef,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  version,
+  Suspense,
+  StrictMode,
+} = React;
